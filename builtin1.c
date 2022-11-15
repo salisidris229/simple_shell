0d9e9c92c2977e95fe1e49179be661c5ff91359f
Keaobaka Matlhola <111046000+kcmatlhola92@users.noreply.github.com>
2022-11-15 14:57:12 +0200

    Update builtin1.c

--- builtin1.c
+++ builtin1.c
@@ -2,10 +2,10 @@
 
 /**
  * _myhistory - displays the history list, one command by line, preceded
- *              with line numbers, starting at 0.
+ * with line numbers, starting at 0.
  * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
+ * constant function prototype.
+ * Return: Always 0
  */
 int _myhistory(info_t *info)
 {
@@ -84,7 +84,7 @@ int print_alias(list_t *node)
  * _myalias - mimics the alias builtin (man alias)
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
- *  Return: Always 0
+ * Return: Always 0
  */
 int _myalias(info_t *info)
 {
