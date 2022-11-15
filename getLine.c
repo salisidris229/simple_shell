593edc6d02c3ad475f62287e5209b59cbefd1a9f
Keaobaka Matlhola <111046000+kcmatlhola92@users.noreply.github.com>
2022-11-15 15:20:18 +0200

    Update getLine.c

--- getLine.c
+++ getLine.c
@@ -6,7 +6,7 @@
  * @buf: address of buffer
  * @len: address of len var
  *
- * Return: bytes read
+ * Return: read bytes
  */
 ssize_t input_buf(info_t *info, char **buf, size_t *len)
 {
@@ -48,7 +48,7 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
  * get_input - gets a line minus the newline
  * @info: parameter struct
  *
- * Return: bytes read
+ * Return: read bytes
  */
 ssize_t get_input(info_t *info)
 {
