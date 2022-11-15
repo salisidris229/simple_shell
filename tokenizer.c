6158abb45cafd0f47f5885b626b8330d9b49b027
Keaobaka Matlhola <111046000+kcmatlhola92@users.noreply.github.com>
2022-11-15 15:51:25 +0200

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -2,11 +2,10 @@
 
 /**
  * **strtow - splits a string into words. Repeat delimiters are ignored
- * @str: the input string
- * @d: the delimeter string
+ * @str: the input of string
+ * @d: the delimeter of string
  * Return: a pointer to an array of strings, or NULL on failure
  */
-
 char **strtow(char *str, char *d)
 {
 	int i, j, k, m, numwords = 0;
